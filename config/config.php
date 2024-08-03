66f40e47d5092bf762c9025a3c3f6cdda1259759
Stanislas HOUETO <stanhoueto2002@gmail.com>
2024-08-03 13:04:14 +0100

    init: config file with the basis constants

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+define('SITE_NAME', 'my-super-site');
+
+
+//App root
+define("APP_ROOT", dirname(dirname(__FILE__)));
+define("URL_ROOT", "/");
+define("URL_SUBFOLDER", "");
+
+
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "hahahaha");
+define("DB_NAME", "supersitedb");
