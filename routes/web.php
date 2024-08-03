a8652e375f3e9be0e2d86fe9de8894bb973ffc6f
Stanislas HOUETO <stanhoueto2002@gmail.com>
2024-08-03 20:52:29 +0100

    Feat: route + view + controller for test

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
-$routes->add(
-        "product", 
+$routes->add("product", 
         new Route(constant('URL_SUBFOLDER').'/product/{id}', 
               array('controller' => 'ProductController', 'method' => 'showAction'), 
               array('id' => '[0-9]+')));
+
+$routes->add('homepage', 
+  new Route(constant('URL_SUBFOLDER').'/', array('controller' => 'PageController', 'method' => 'indexAction'), array())
+);
